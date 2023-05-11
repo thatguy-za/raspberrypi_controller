48c1fb573c978fba82dcd65c6c6ef00c7e79eb9c
Tristam <69636837+thatguy-za@users.noreply.github.com>
2023-05-11 07:37:59 +0100

    Update shutdown_only_controller.py

--- shutdown_only_controller.py
+++ shutdown_only_controller.py
@@ -1,26 +1,15 @@
-import RPi.GPIO as GPIO
+#This script uses the gpiozero library to shut down a pi. Depending on your distro, you might need to run the poweroff command as sudo.
+from gpiozero import Button
+import time
 import os
 
-# Define the GPIO pins and blink time that you will use.
+# The pi will shutdown when pin 21 is connected to ground
+shutdown_button_pin = Button(21, pull_up=True)
 
-shutdown_pin = 21
-
-# Setup the pin to use internal pullup resistors and the operating mode (input/output).
-
-GPIO.setmode(GPIO.BCM)
-GPIO.setup(shutdown_pin, GPIO.IN, pull_up_down = GPIO.PUD_UP)
-
-# Defines the Shutdown and Restart functions that will be called when the buttons are pushed
-
-def Shutdown(channel):
-   os.system("sudo poweroff")
-
-# Add our function to execute when a button is pressed.
-# I've set the bouncetime to 10 seconds so you can hold the button down while you wait for the LED's to blink
-
-GPIO.add_event_detect(2, GPIO.FALLING, callback = Shutdown, bouncetime = 10000)
-
-# Now wait!
-
-while 1:
-   time.sleep(1)
+while True:
+     if shutdown_button_pin.is_pressed:
+        time.sleep(2)
+        # wbutton must be pressed for 2 seconds
+        if shut_But.is_pressed:
+            os.system("poweroff")
+            time.sleep(1)
