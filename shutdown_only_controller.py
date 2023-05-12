150f3bb8e247819cdd89b1d15106002e46184728
Tristam <69636837+thatguy-za@users.noreply.github.com>
2023-05-12 17:25:54 +0100

    Update shutdown_only_controller.py

--- shutdown_only_controller.py
+++ shutdown_only_controller.py
@@ -1,11 +1,12 @@
 from gpiozero import Button
 from signal import pause
+import os
 
 # The pi will shutdown when pin 21 is connected to ground
 shutdown_button_pin = Button(21, hold_time=2)
 
 def shutdown():
-    check_call(['poweroff'])
+    os.system("poweroff")
 
 shutdown_button_pin.when_held = shutdown
 
